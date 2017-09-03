4e0970f81fa88b55c9c3df51b98dde03872af99c
Marcus Smith <marcussmith0711@gmail.com>
2017-09-03 01:23:38 -0500

    added node version

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const app = require("express")();
+const express = require("express");
 const path = require("path");
+const app = express();
+
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname));
